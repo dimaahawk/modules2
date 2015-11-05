e552750fc3643d07b77c3227337f0e206f944cc5
dimaa <you@example.com>
2015-11-05 21:49:30 +1100

    Securing tweetstuff.py by splitting API keys into a separate file. That file was added to the .gitignore in a previous commit. I believe the one right before this one.

--- tweetstuff.py
+++ tweetstuff.py
@@ -2,13 +2,21 @@ import tweepy
 from sopel import *
 import random
 
+try:  # Check for key file
+	from twitter_keys import keys
+except ImportError:  # Else...well get one
+	print 'Get your keys sorted....'
+
 @module.commands('tweetstuff')
 def tweetstuff(bot, trigger):
-	# Replace the below variables with your keys & tokens
-	CONSUMER_KEY = 'your_key'
-	CONSUMER_SECRET = 'your_key'
-	ACCESS_TOKEN = 'your_key'
-	ACCESS_TOKEN_SECRET = 'your_key'
+
+	# Storing keys in a separate file for security reasons
+	# Check bottom of key file structure
+	CONSUMER_KEY = keys['CONSUMER_KEY']
+	CONSUMER_SECRET = keys['CONSUMER_SECRET']
+	ACCESS_TOKEN = keys['ACCESS_TOKEN']
+	ACCESS_TOKEN_SECRET = keys['ACCESS_TOKEN_SECRET']
+
 
 	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -24,4 +32,20 @@ def tweetstuff(bot, trigger):
 			api.update_status(status=tweet)
 			bot.say('Tweeted')
 
-# Version 1.0
+# Version 2.0
+
+'''
+Key file name : twitter_keys.py
+contains a dictionary called keys
+
+keys = {
+	'CONSUMER_KEY': 'put your key here',
+	'CONSUMER_SECRET': 'put your key here',
+	'ACCESS_TOKEN': 'put your key here',
+	'ACCESS_TOKEN_SECRET': 'put your key hereL'
+}
+
+
+'''
+
+
