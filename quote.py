89adf4ade980f540668effacf3a9e20babef8676
dimaahawk <moebob24@gmail.com>
2015-12-09 20:51:16 +1100

    test logging library

--- quote.py
+++ quote.py
@@ -1,13 +1,24 @@
 from sopel import *
 import random
+import logging
+import os
 
-qdb = "/home/dimaa/.sopel/quote.db"
+box_user = os.getlogin()
+
+
+logging.basicConfig(
+	filename='/home/%s/mylogs/adopsbot/quote.log' % box_user,
+	level=logging.DEBUG
+	)
+
+qdb = '/home/%s/.sopel/quote.db' % box_user
 
 @module.commands('addquote')
 def addquote(bot, trigger):
         quote = trigger.group(2)
         f = open(qdb, 'a')
         f.write("\n%s" % quote)
+        logging.info('%s added a quote --> %s' % (trigger.nick(), quote))
         f.close()
         bot.reply('Added it...')
 
